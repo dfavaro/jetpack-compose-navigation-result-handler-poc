157ff648657b3b9f9cdb8015a14160944982ba35
Daniele Favaro <daniele.favaro.1@gmail.com>
2025-02-03 10:53:40 +0100

    First commit

---
+++ app/src/main/java/com/danielefavaro/navresulthandlerpoc/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.danielefavaro.navresulthandlerpoc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.danielefavaro.navresulthandlerpoc.ui.theme.NavResultHandlerPoCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavResultHandlerPoCTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NavResultHandlerPoCTheme {
+        Greeting("Android")
+    }
+}
